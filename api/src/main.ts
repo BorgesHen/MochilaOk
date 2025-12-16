d1fac483617ec943e4db176b1a79ce34c4509574
Henrique Carboni Borges <borgesh989@gmail.com>
2025-12-16 17:40:52 -0300

    Track api e web no repo principal

---
+++ api/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credencials: true,
+  });
+
+  await app.listen(3001);
+}
+bootstrap();
